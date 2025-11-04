7a37476f58ff088362f67bead30bd04255811cdc
HenriqueFerr <ferreirahenrique9501@gmail.com>
2025-11-04 11:56:36 -0300

    feat/adicionado o enum validateRange para humor, energia e estresse

--- src/service/answerService.js
+++ src/service/answerService.js
@@ -1,9 +1,20 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+function validateRange( value, min, max, fieldname) {
+    if (typeof value !== 'number' || value < min || value > max) {
+        throw new Error(`VALIDATION_FIELD: O campo ${fieldname} deve ser um número entre ${min} e ${max}.`);
+    }
+}
+
 const answerService = {
     async createResposta(answerData, userId) {
         const { humor, energia, estresse, resumo } = answerData;
+
+        validateRange(humor, 0, 5, 'humor');
+        validateRange(energia, 0, 4, 'energia');
+        validateRange(estresse, 0, 4, 'estresse');
+
         try {
             const newResposta = await prisma.respostas.create({
                 data: {
